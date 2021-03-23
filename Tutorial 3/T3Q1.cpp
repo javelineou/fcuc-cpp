fe15ae36ca7d2cdef71bf7a33a4a99e473f8ba2a
javelineou <63649797+javelineou@users.noreply.github.com>
2021-03-23 17:14:10 +0700

    Add files via upload

---
+++ Tutorial 3/T3Q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i;
+    cout << "Table of 10 \n";
+    for (i=1; i<11; i++){
+        cout<<"10 X "<<i<<" = "<<10*i<<"\n";
+    }
+}
---
+++ Tutorial 3/T3Q2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i;
+    for (i=0; i<21; i+=2){
+        if(i!=0){
+            cout<<"The even number is: "<<i<<"\n";
+        }
+    }
+}
